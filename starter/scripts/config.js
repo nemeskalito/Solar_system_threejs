d6e8321f266fe672886b270beb417924a5d238a0
nemeskalito <borisenko.igor2021@yandex.ru>
2026-01-17 16:31:22 +0300

    first commit

---
+++ starter/scripts/config.js
@@ -0,0 +1,23 @@
+const config = {
+	shadows: {
+		resolution: 2048,
+		noramlBias: .05
+	},
+	lighting: {
+		ambientIntensity: .4,
+		directionalIntensity: 1.75,
+		directionalPosition: [ 5, 10, 7 ]
+	},
+	camera: {
+		fov: 60,
+		position: [ 4, 4, 8 ],
+		near: .1,
+		far: 1000
+	},
+	material: {
+		roughness: .7,
+		metallness: 0
+	}
+}
+
+export default config
